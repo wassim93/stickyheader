62cda016a45c97d5c31fa4bcadd90079d7de3920
wassim <b.wassim1993@hotmail.fr>
2021-10-07 17:40:24 +0100

    Initial Commit

---
+++ stickyheader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  stickyheader
+//
+//  Created by wassim on 7/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ stickyheader/stickyheaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  stickyheaderApp.swift
+//  stickyheader
+//
+//  Created by wassim on 7/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct stickyheaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
