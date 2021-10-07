d718d4c72b9e8515996b1bffa816edd86d15f4dd
wassim <b.wassim1993@hotmail.fr>
2021-10-07 18:01:49 +0100

    project init

--- stickyheader/ContentView.swift
+++ stickyheader/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Home()
+    }
+}
+
+struct Home: View {
+
+    var body: some View {
+        VStack{
+            Image("naruto")
+                .resizable()
+                .frame(height: UIScreen.main.bounds.height / 2)
+            VStack(alignment: .leading) {
+                Text("Naruto")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .padding([.top,.bottom])
+
+                Text(textDetail)
+            }
+
+            Spacer()
+        }.edgesIgnoringSafeArea(.top)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.sizeThatFits)
     }
 }
